4091c4ba2f799eb3a192924b525f194654d4caed
samiramrullah <samiramrullah@gmail.com>
2022-10-18 00:44:19 +0530

    btn design

--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -1,8 +1,8 @@
-import { Text, Button, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 import PrimaryButton from '../Components/PrimaryButton';
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.innerContainer}>
             <TextInput placeholder='Enter Number' />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
@@ -11,3 +11,17 @@ const StartGameScreen = () => {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    innerContainer: {
+        // flex: 1,
+        padding: 10,
+        marginTop: 85,
+        backgroundColor: 'purple',
+        marginHorizontal: 10,
+        borderRadius: 10,
+        elevation: 8,
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 5
+    }
+})
